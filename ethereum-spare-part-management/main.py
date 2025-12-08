fc1853d25dc61fbbd2d09b6b38017b43f4eb63da
Natalia <npieczko23@gmail.com>
2025-12-08 17:14:43 +0100

    Change folder name to ethereum-spare-part-management from spare-part-management

---
+++ ethereum-spare-part-management/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ethereum-spare-part-management!")
+
+
+if __name__ == "__main__":
+    main()
--- spare-part-management/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from spare-part-management!")
-
-
-if __name__ == "__main__":
-    main()
